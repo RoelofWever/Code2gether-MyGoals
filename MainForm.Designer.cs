a2fc8a9d01c25f08fc9526ca771dec496adb9384
Roelof Wever <roelof@wever-home.nl>
2020-11-23 20:49:35 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace MyGoals
+{
+    partial class Mainform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TcMain = new System.Windows.Forms.TabControl();
+            this.TpOverview = new System.Windows.Forms.TabPage();
+            this.TcGoals = new System.Windows.Forms.TabControl();
+            this.TpCreateGoal = new System.Windows.Forms.TabPage();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.TbStep = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TbNameOfGoal = new System.Windows.Forms.TextBox();
+            this.LbGoalSteps = new System.Windows.Forms.ListBox();
+            this.LblNameOfGoal = new System.Windows.Forms.Label();
+            this.TcMain.SuspendLayout();
+            this.TpOverview.SuspendLayout();
+            this.TpCreateGoal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TcMain
+            // 
+            this.TcMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TcMain.Controls.Add(this.TpOverview);
+            this.TcMain.Controls.Add(this.TpCreateGoal);
+            this.TcMain.ItemSize = new System.Drawing.Size(100, 22);
+            this.TcMain.Location = new System.Drawing.Point(0, 0);
+            this.TcMain.Name = "TcMain";
+            this.TcMain.Padding = new System.Drawing.Point(3, 3);
+            this.TcMain.SelectedIndex = 0;
+            this.TcMain.Size = new System.Drawing.Size(524, 391);
+            this.TcMain.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.TcMain.TabIndex = 4;
+            // 
+            // TpOverview
+            // 
+            this.TpOverview.Controls.Add(this.TcGoals);
+            this.TpOverview.Location = new System.Drawing.Point(4, 26);
+            this.TpOverview.Name = "TpOverview";
+            this.TpOverview.Padding = new System.Windows.Forms.Padding(3);
+            this.TpOverview.Size = new System.Drawing.Size(516, 361);
+            this.TpOverview.TabIndex = 0;
+            this.TpOverview.Text = "Overview";
+            this.TpOverview.UseVisualStyleBackColor = true;
+            // 
+            // TcGoals
+            // 
+            this.TcGoals.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TcGoals.Location = new System.Drawing.Point(0, 0);
+            this.TcGoals.Name = "TcGoals";
+            this.TcGoals.SelectedIndex = 0;
+            this.TcGoals.Size = new System.Drawing.Size(512, 357);
+            this.TcGoals.TabIndex = 6;
+            // 
+            // TpCreateGoal
+            // 
+            this.TpCreateGoal.BackColor = System.Drawing.Color.DarkGray;
+            this.TpCreateGoal.Controls.Add(this.BtnStart);
+            this.TpCreateGoal.Controls.Add(this.BtnAdd);
+            this.TpCreateGoal.Controls.Add(this.TbStep);
+            this.TpCreateGoal.Controls.Add(this.label2);
+            this.TpCreateGoal.Controls.Add(this.label1);
+            this.TpCreateGoal.Controls.Add(this.TbNameOfGoal);
+            this.TpCreateGoal.Controls.Add(this.LbGoalSteps);
+            this.TpCreateGoal.Controls.Add(this.LblNameOfGoal);
+            this.TpCreateGoal.Location = new System.Drawing.Point(4, 26);
+            this.TpCreateGoal.Name = "TpCreateGoal";
+            this.TpCreateGoal.Padding = new System.Windows.Forms.Padding(3);
+            this.TpCreateGoal.Size = new System.Drawing.Size(516, 361);
+            this.TpCreateGoal.TabIndex = 1;
+            this.TpCreateGoal.Text = "Create a goal";
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Enabled = false;
+            this.BtnStart.Location = new System.Drawing.Point(377, 319);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(75, 23);
+            this.BtnStart.TabIndex = 5;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnDone_Click);
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Enabled = false;
+            this.BtnAdd.Location = new System.Drawing.Point(377, 41);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(75, 23);
+            this.BtnAdd.TabIndex = 3;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // TbStep
+            // 
+            this.TbStep.BackColor = System.Drawing.SystemColors.Menu;
+            this.TbStep.Location = new System.Drawing.Point(132, 42);
+            this.TbStep.Name = "TbStep";
+            this.TbStep.Size = new System.Drawing.Size(239, 20);
+            this.TbStep.TabIndex = 2;
+            this.TbStep.TextChanged += new System.EventHandler(this.TbStep_TextChanged);
+            this.TbStep.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TbStep_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Steps to reach goal";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Steps to reach goal";
+            // 
+            // TbNameOfGoal
+            // 
+            this.TbNameOfGoal.BackColor = System.Drawing.SystemColors.Menu;
+            this.TbNameOfGoal.Location = new System.Drawing.Point(132, 16);
+            this.TbNameOfGoal.Name = "TbNameOfGoal";
+            this.TbNameOfGoal.Size = new System.Drawing.Size(239, 20);
+            this.TbNameOfGoal.TabIndex = 1;
+            this.TbNameOfGoal.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TbNameOfGoal_KeyDown);
+            // 
+            // LbGoalSteps
+            // 
+            this.LbGoalSteps.BackColor = System.Drawing.SystemColors.Menu;
+            this.LbGoalSteps.FormattingEnabled = true;
+            this.LbGoalSteps.Location = new System.Drawing.Point(132, 65);
+            this.LbGoalSteps.Name = "LbGoalSteps";
+            this.LbGoalSteps.Size = new System.Drawing.Size(239, 277);
+            this.LbGoalSteps.TabIndex = 4;
+            // 
+            // LblNameOfGoal
+            // 
+            this.LblNameOfGoal.AutoSize = true;
+            this.LblNameOfGoal.Location = new System.Drawing.Point(23, 23);
+            this.LblNameOfGoal.Name = "LblNameOfGoal";
+            this.LblNameOfGoal.Size = new System.Drawing.Size(70, 13);
+            this.LblNameOfGoal.TabIndex = 0;
+            this.LblNameOfGoal.Text = "Name of goal";
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 397);
+            this.Controls.Add(this.TcMain);
+            this.Name = "Mainform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My goals";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Mainform_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TcMain.ResumeLayout(false);
+            this.TpOverview.ResumeLayout(false);
+            this.TpCreateGoal.ResumeLayout(false);
+            this.TpCreateGoal.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl TcMain;
+        private System.Windows.Forms.TabPage TpOverview;
+        private System.Windows.Forms.TabPage TpCreateGoal;
+        private System.Windows.Forms.TextBox TbStep;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TbNameOfGoal;
+        private System.Windows.Forms.ListBox LbGoalSteps;
+        private System.Windows.Forms.Label LblNameOfGoal;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.TabControl TcGoals;
+        private System.Windows.Forms.Button BtnStart;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+
+
+
+//REMARK
+// in design time the following properties are set for TcMain
+// to hide the tabs from the tabcontrol
+// TcMain.Appearance = TabAppearance.FlatButtons;
+// TcMain.ItemSize = new Size(0, 1);
+// they are set again in the form loaded to make sure hey stay hidden
+
+
+
+namespace MyGoals
+{
+
+    public partial class Mainform : Form
+    {
+        
+        private const string ProgressBarPrefix = "Pb";
+        private const string CheckListBoxPrefix = "Clb";
+        private const string ButtonPrefix = "Btn";
+
+        private string GetXMlFileName()
+        {
+            var filename = "Goals.xml";
+            var currentDirectory = Directory.GetCurrentDirectory();
+            return Path.Combine(currentDirectory, filename);
+        }
+
+        private class MyListboxItem
+        {
+            public string Caption { get; set; }
+            public int Index { get; set; }
+        }
+
+        // use a BindingList so we can display steps in a listbox
+        private readonly BindingList<MyListboxItem> MyLbItems = new BindingList<MyListboxItem>();
+
+        public Mainform()
+        {
+            InitializeComponent();
+            LbGoalSteps.DataSource = MyLbItems;
+            LbGoalSteps.DisplayMember = "Caption";
+            LbGoalSteps.ValueMember = "Index";
+        }
+
+
+        // Create a step and add it to our own list
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            MyLbItems.Add(new MyListboxItem { Index = MyLbItems.Count, Caption = TbStep.Text });
+            TbStep.Clear();
+            TbStep.Focus();
+            EnableBtnDone();
+        }
+
+        private void EnableBtnDone()
+        {
+            BtnStart.Enabled = MyLbItems.Count > 0 && !string.IsNullOrEmpty(TbNameOfGoal.Text);
+        }
+
+        private void TbStep_TextChanged(object sender, EventArgs e)
+        {
+            BtnAdd.Enabled = (sender as TextBox).Text != "";
+        }
+
+        private void TbStep_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                BtnAdd_Click(null, null); 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadXml();
+            // if ther is no xml then there are no Goals to show 
+            // so let the users create one first
+            if (TcGoals.TabPages.Count == 0)
+                TcMain.SelectedIndex = 1;
+        }
+
+        // create a tabpage with a few controls so the use can adjust the progress
+        private TabPage MakeGoalTabPage( string Caption, int Index)
+        {
+            TabPage _tpTmp = new TabPage()
+            {
+                Text = Caption,
+                BackColor = Color.DarkGray,
+        };
+
+            ProgressBar Pb = new ProgressBar()
+            {
+                Name = ProgressBarPrefix + Index.ToString(),
+                Parent = _tpTmp,
+                Left = 10,
+                Top = 10,
+                Height = 15,
+                Width = 450,
+            };
+            CheckedListBox Clb = new CheckedListBox()
+            {
+                Name = CheckListBoxPrefix + Index.ToString(),
+                BackColor = SystemColors.Menu,
+                Parent = _tpTmp,
+                Left = 10,
+                Top = 38,
+                Width = 237,
+                Height = 277,
+            };
+
+            Button Btn = new Button()
+            {
+                Name = ButtonPrefix + Index.ToString(),
+                UseVisualStyleBackColor = true,
+                Parent = _tpTmp,
+                Left = 250,
+                Top = 290,
+                Text = "Done",
+                Enabled = false,
+            };
+
+            // add the steps to the checklistbox
+            for (int i = 0; i < MyLbItems.Count; i++)
+            {
+                Clb.Items.Add(MyLbItems[i].Caption);
+            }
+
+            Clb.ItemCheck += new ItemCheckEventHandler(this.ItemChecked);
+            Pb.Maximum = Clb.Items.Count;
+            Btn.Click += CloseTab;
+            return _tpTmp;
+        }
+
+        // user checked a step of a goal
+        private void ItemChecked(object sender, ItemCheckEventArgs e)
+        {
+            // find the active tabpage
+            // this could also be done by this
+            // TabPage _tpTmp = tcGoals.SelectedTab;
+            TabPage _tpTmp = ((CheckedListBox)sender).Parent as TabPage;
+
+            // get the name of the component that triggerd this event 
+            // we know it has to be a CheckedListBox because thats the control we added the event to
+            string _componentname = ((CheckedListBox)sender).Name;
+            // now find the progressbar on the tabpage change the component prefix to the one used for the progressbar
+            ProgressBar Pb = (ProgressBar)_tpTmp.Controls.Find(_componentname.Replace(CheckListBoxPrefix, ProgressBarPrefix), false).FirstOrDefault();
+
+            // find out how many checkboxes are checked
+            // the amount will be the amout before the user did something
+
+            // from MS Documentation
+            // Remarks
+            //  The check state is not updated until after the ItemCheck event occurs.
+
+            var _amountChecked = ((CheckedListBox)sender).CheckedItems.Count;
+            // Did the user add or remove a check?
+            if (e.NewValue == CheckState.Checked)
+            {
+                _amountChecked++;
+            }
+            else
+            {
+                _amountChecked--;
+            }
+
+            // Adjust the progressbar acordingly
+            if (Pb != null)
+            {
+                Pb.Value = _amountChecked;
+            }
+            Button btn = (Button)_tpTmp.Controls.Find(_componentname.Replace(CheckListBoxPrefix, ButtonPrefix), false).FirstOrDefault();
+            btn.Enabled = Pb.Value == Pb.Maximum;
+        }
+
+        // user has defined a goal put it in view so the user can keep track of its progress
+        private void BtnDone_Click(object sender, EventArgs e)
+        {
+            TcMain.SelectedIndex = 0;
+            TcGoals.TabPages.Add(MakeGoalTabPage(TbNameOfGoal.Text, TcGoals.TabPages.Count));
+        }
+
+        // save all goals to a XML file so we can load them again the next time
+        private void Mainform_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // and save it 
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                Indent = true,
+                IndentChars = ("    "),
+                CloseOutput = true,
+                OmitXmlDeclaration = true
+            };
+
+            // with the using we don't have to worry about disposing the things we created they will be desposed once they are out of scope
+            using (XmlWriter writer = XmlWriter.Create(GetXMlFileName(), settings))
+            {
+                writer.WriteStartElement("Goals");
+                for (int idx = 0; idx < TcGoals.TabPages.Count; idx++)
+                {
+                    writer.WriteStartElement("Goal");
+                    writer.WriteElementString("Name", TcGoals.TabPages[idx].Text);
+                    CheckedListBox Clb = (CheckedListBox)TcGoals.TabPages[idx].Controls.Find(CheckListBoxPrefix + idx.ToString(), false).FirstOrDefault();
+                    if (Clb != null)
+                    {
+                        writer.WriteStartElement("Steps");
+                        for (int idy = 0; idy < Clb.Items.Count; idy++)
+                        {
+                            writer.WriteStartElement("Step");
+                            writer.WriteElementString("Name", Clb.Items[idy].ToString());
+                            writer.WriteElementString("Done", Clb.GetItemChecked(idy).ToString());
+                            writer.WriteEndElement();
+                        }
+                        writer.WriteEndElement();
+                    }
+                    writer.WriteEndElement();
+                }
+                writer.Flush();
+            }
+        }
+
+        private void LoadXml()
+        {
+            if (File.Exists(GetXMlFileName()))
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(GetXMlFileName());
+                XmlNode root = doc.FirstChild;
+
+                //Display the contents of the child nodes.
+                if (root.HasChildNodes)
+                {
+                    for (int i = 0; i < root.ChildNodes.Count; i++)
+                    {
+                        if (root.ChildNodes[i].HasChildNodes)
+                        {
+                            TabPage _tp = MakeGoalTabPage(root.ChildNodes[i].ChildNodes[0].InnerText, TcGoals.TabPages.Count);
+                            ProgressBar _pb = (ProgressBar)_tp.Controls.Find(ProgressBarPrefix + TcGoals.TabPages.Count.ToString(), true).FirstOrDefault();
+                            CheckedListBox _clb = (CheckedListBox)_tp.Controls.Find(CheckListBoxPrefix + TcGoals.TabPages.Count.ToString(), true).FirstOrDefault();
+                            for (int J = 0; J < root.ChildNodes[i].ChildNodes.Count; J++)
+                            {
+                                if (_pb != null && _clb != null)
+                                {
+                                    _pb.Maximum = root.ChildNodes[i].ChildNodes[J].ChildNodes.Count;
+                                    for (int Y = 0; Y < root.ChildNodes[i].ChildNodes[J].ChildNodes.Count; Y++)
+                                    {
+                                        if (root.ChildNodes[i].ChildNodes[J].ChildNodes[Y].ChildNodes.Count > 0)
+                                        {
+                                            _clb.Items.Add(root.ChildNodes[i].ChildNodes[J].ChildNodes[Y].ChildNodes[0].InnerText,
+                                                           bool.Parse(root.ChildNodes[i].ChildNodes[J].ChildNodes[Y].ChildNodes[1].InnerText));
+                                        }
+                                    }
+                                }
+                            }
+                            TcGoals.TabPages.Add(_tp);
+                        }
+                    }
+                }
+            }
+        }
+
+        // let the user add a new goal but make sure everything is cleared first
+        private void CreateGoalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TbNameOfGoal.Clear();
+            TbStep.Clear();
+            MyLbItems.Clear();
+            TcMain.SelectedIndex = 1;
+        }
+
+        private void TbNameOfGoal_KeyDown(object sender, KeyEventArgs e)
+        {
+            EnableBtnDone();
+        }
+
+        private void CloseTab(object sender, EventArgs e)
+        {
+            TcGoals.SelectedTab.Dispose();
+        }
+    }
+}
